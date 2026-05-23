cc83707d8cb0538d3530e7471e75e32d7bf402a6
Daniel Abrahao <daniel@preface.com.br>
2026-05-23 09:44:16 -0300

    Correção no teste

--- tests_alta_ia_autorizado/test_alta_ia_autorizado.py
+++ tests_alta_ia_autorizado/test_alta_ia_autorizado.py
@@ -11,13 +11,3 @@ def test_dividir_por_zero():
     with pytest.raises(ValueError):
         dividir(10, 0)
  
-dentro da pasta tests_alta_ia_autorizado
- 
-import pytest # type: ignore
-from src_baixa.app import somar, dividir
- 
-def test_somar():
-    assert somar(2, 3) == 5
- 
-dentro da pasta tests_baixa
- 
